e9f2fa34ced34b9729c2f22a9e82ef8826cf7680
Satoshi550 <78008562+Satoshi550@users.noreply.github.com>
2023-11-27 09:53:56 -0600

    Initial Commit

---
+++ MicroUi December/MicroUi_DecemberApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroUi_DecemberApp.swift
+//  MicroUi December
+//
+//  Created by Satoshi Mitsumori on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MicroUi_DecemberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
